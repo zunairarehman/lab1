d5232e44279439be382435230fdfc3ea1b474a3b
zunairarehman <107191132+zunairarehman@users.noreply.github.com>
2024-11-16 11:51:46 +0500

    lab 1 practice

---
+++ lab1 practice.py
@@ -0,0 +1,13 @@
+#my first program
+print("HELLO WORLD")
+#my second program
+print("WELCOME TO THE\nWORLD OF PROGRAMMING")
+#bio data
+name=input("Enter name:")
+age=input("Enter age:")
+city=input("Enter city:")
+phone=input("Enter phone number:")
+uniname=input("Enter university name:")
+print("\t***BIO-DATA***")
+print("\t--------------")
+print(name,"\n",age,"\n",city,"\n",phone,"\n",uniname)
